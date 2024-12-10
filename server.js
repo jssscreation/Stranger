399852ea9cef87f4f1857b524f1f1868ac1746f7
jssscreation <50180732+jssscreation@users.noreply.github.com>
2024-12-10 17:19:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,46 +1,25 @@
 const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
-
+const WebSocket = require('ws');
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
+const server = require('http').createServer(app);
+
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', (ws) => {
+    console.log('New user connected.');
+
+    ws.on('message', (message) => {
+        // Broadcast to all other clients
+        wss.clients.forEach((client) => {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                client.send(message);
+            }
+        });
+    });
+
+    ws.on('close', () => console.log('User disconnected.'));
+});
 
 app.use(express.static('public'));
 
-let waitingUser = null;
-
-io.on('connection', (socket) => {
-    console.log('A user connected:', socket.id);
-
-    socket.on('disconnect', () => {
-        console.log('A user disconnected:', socket.id);
-        if (waitingUser === socket) {
-            waitingUser = null;
-        }
-    });
-
-    // When a user is ready to connect
-    socket.on('ready', () => {
-        if (waitingUser) {
-            // Connect the two users
-            socket.emit('offer', { offerTo: waitingUser.id });
-            waitingUser.emit('offer', { offerTo: socket.id });
-            waitingUser = null;
-        } else {
-            waitingUser = socket;
-        }
-    });
-
-    // Forward WebRTC signaling data
-    socket.on('signal', (data) => {
-        io.to(data.to).emit('signal', {
-            from: socket.id,
-            signal: data.signal,
-        });
-    });
-});
-
-server.listen(5555, () => {
-    console.log('Server is running on http://localhost:5555');
-});
+server.listen(3000, () => console.log('Server running on port 3000'));
